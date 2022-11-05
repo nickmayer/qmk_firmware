4710a4ec843bcfe25e2d68a9111d342a98c38b26
Nick Mayer <nick@lastacorn.com>
2022-11-05 11:30:30 -0600

    Add RGB led strip

--- keyboards/lastacorn/radicle/mcuconf.h
+++ keyboards/lastacorn/radicle/mcuconf.h
@@ -2,14 +2,27 @@
 
 #include_next "mcuconf.h"
 
+// Chibios system tick timer
+#undef STM32_ST_USE_TIMER
+#define STM32_ST_USE_TIMER      5
+#define CH_CFG_ST_RESOLUTION    32
+
+// I2C
 #undef STM32_I2C_USE_I2C1
 #define STM32_I2C_USE_I2C1  TRUE
-
 #undef STM32_I2C_USE_I2C2
-#define STM32_I2C_USE_I2C2 TRUE
+#define STM32_I2C_USE_I2C2  TRUE
 
-#undef STM32_PWM_USE_TIM5
-#define STM32_PWM_USE_TIM5  TRUE
+// PWM
+#undef STM32_PWM_USE_TIM2
+#define STM32_PWM_USE_TIM2  TRUE
+#undef STM32_PWM_USE_TIM3
+#define STM32_PWM_USE_TIM3  TRUE
 
+// ADC
 #undef STM32_ADC_USE_ADC1
 #define STM32_ADC_USE_ADC1  TRUE
+
+// SPI
+#undef STM32_SPI_USE_SPI1
+#define STM32_SPI_USE_SPI1  TRUE
--- keyboards/lastacorn/radicle/radicle.c
+++ keyboards/lastacorn/radicle/radicle.c
@@ -27,3 +27,17 @@ joystick_config_t joystick_axes[JOYSTICK_AXES_COUNT] = {
     JOYSTICK_AXIS_IN(PIN_JOY_Z, 1023, 512, 0),
     JOYSTICK_AXIS_IN(PIN_JOY_R, 1023, 512, 0),
 };
+
+led_config_t g_led_config = { {
+    /* Key Matrix to LED index. */
+    {   2,   3,  5 },
+    {   1,   4,  0 },
+}, {
+    /* LED index to physical position. */
+    {   0,  42 }, {   0,  21 }, {   0,   0 },
+    {  18,   0 }, {  18,  21 }, {  18,  42 },
+}, {
+    /* LED index to flag. */
+    LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_ALL,
+    LED_FLAG_MODIFIER, LED_FLAG_KEYLIGHT, LED_FLAG_ALL,
+} };
